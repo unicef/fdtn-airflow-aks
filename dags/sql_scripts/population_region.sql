cbfb1ac1b2698aaa9b080e604508aa229262a0f0
unicef <hugo.ruiz.verastegui@gmail.com>
2023-07-13 08:44:44 +0000

    Update from https://github.com/unicef/hd4d_ppt/commit/59db4f01de6a8bd0199985692020d118d9833689

--- dags/sql_scripts/population_region.sql
+++ dags/sql_scripts/population_region.sql
@@ -8,18 +8,18 @@ CREATE TABLE population_by_region as (
 SELECT disasters_hex.event_id ,
 adm2_hex.gid2 AS gid2,
 eapro_adm2."NAME_2" AS "NAME_2", 
-coalesce(sum(population_crosstab.children_under_five),0) AS children_under_five, 
-coalesce(sum(population_crosstab.elderly_60_plus),0) AS elderly_60_plus, 
-coalesce(sum(population_crosstab.men),0) AS men, 
-coalesce(sum(population_crosstab.women),0) AS women, 
-coalesce(sum(population_crosstab.women_of_reproductive_age_15_49),0) AS women_of_reproductive_age_15_49, 
-coalesce(sum(population_crosstab.youth_15_24),0) AS youth_15_24, 
-coalesce(sum(population_crosstab.general),0) AS general, 
+coalesce(sum(pc.children_under_five),0) AS children_under_five, 
+coalesce(sum(pc.elderly_60_plus),0) AS elderly_60_plus, 
+coalesce(sum(pc.men),0) AS men, 
+coalesce(sum(pc.women),0) AS women, 
+coalesce(sum(pc.women_of_reproductive_age_15_49),0) AS women_of_reproductive_age_15_49, 
+coalesce(sum(pc.youth_15_24),0) AS youth_15_24, 
+coalesce(sum(pc.general),0) AS general, 
 coalesce(sum(schools.count),0) AS school_count ,
 coalesce(sum(hospitals.count),0) AS hospital_count 
 
-FROM population_crosstab 
-LEFT JOIN disasters_hex ON population_crosstab.h3_08 = disasters_hex.h3_08 
+FROM population_crosstab_deduplicated pc
+LEFT JOIN disasters_hex ON pc.h3_08 = disasters_hex.h3_08 
 LEFT JOIN adm2_hex ON disasters_hex.h3_08 = adm2_hex.h3_08 
 LEFT JOIN eapro_adm2 ON adm2_hex.gid2 = eapro_adm2."GID_2" 
 LEFT JOIN schools ON schools.h3_08 = adm2_hex.h3_08 
