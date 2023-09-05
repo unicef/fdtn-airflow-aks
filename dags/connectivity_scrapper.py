fbef873b9564a442c48f0833d176074e4f78f579
huruizverastegui <118810504+huruizverastegui@users.noreply.github.com>
2023-09-05 17:15:43 +0700

    Update connectivity_scrapper.py

--- dags/connectivity_scrapper.py
+++ dags/connectivity_scrapper.py
@@ -47,6 +47,10 @@ loginURL='https://www.facebook.com'
 loginName=os.getenv('META_LOGIN')
 loginPass=os.getenv('META_PASSWORD')
 
+#GET POSTGRES INFO
+#password_postgres=os.getenv('XXX')
+#login_postgres=os.getenv('XXX')
+
 vm_public_ip=os.getenv('VM_PUBLIC_IP')
 
 #list of countries to filter 
@@ -233,13 +237,323 @@ def get_datasets(driver):
     return dataset_id_list
 
 
+def get_already_downloaded():
+    # get the list of already downloaded datasets
+    #to be improved
+
+    engine = create_engine(f'postgresql://{login_postgres}:{password_postgres}@postgres-fdtn.postgres.database.azure.com:5432/postgres')
+    query='select mcd.meta_disaster_id as disaster_id, mch.data_type as data_type,mch.date from private.meta_connectivity_disaster_test mcd left join private.meta_connectivity_hex_test mch on mcd.meta_disaster_id = mch.meta_disaster_id group by 1,2,3 '
+    
+    df_already_in_db=pd.read_sql(query,engine)
+    df_already_in_db['uid']=df_already_in_db['disaster_id'].astype(str) +"-"+ df_already_in_db['data_type']+"-"+df_already_in_db['date'].astype(str)
+    
+    list_already_in_db=list(df_already_in_db['uid'])
+    list_disaster_already_in_db=list(df_already_in_db['disaster_id'].unique())
+
+    # to remove later
+    list_already_in_db=[]
+    list_disaster_already_in_db=[]
+    
+    return (list_already_in_db, list_disaster_already_in_db)
+
+
+
+def download_csvs(driver,datatype):
+    random_sleep(1)
+    htmlsource=driver.page_source
+    random_sleep(1)
+    
+    # get the disaster name 
+    regexp_disaster_name="div aria-level=\"2.*?\" role=\"heading\">(.*?)<\/div><span class=\""
+    try:
+        disaster_name= re.findall(regexp_disaster_name, htmlsource)[0]
+    except:
+        disaster_name=''      
+    
+    #download all csvs in the page
+    #get all the strings to be used to find the csv links via get_element_contains
+    #the format is something like this: "mar 15, 2023 00:00 • 0.5 MB • csv"
+    
+    regexp_download_elements="href=\"\#\">(.*?)<\/a>"
+    elements_list= re.findall(regexp_download_elements, htmlsource)
+    
+    #keep only elements containing csv 
+    csv_elements_list = [x for x in elements_list if "csv" in x]
+    print(csv_elements_list)
+    
+    # keep only elements that are not already in the list 
+    
+    #For each element in the csv_element_list -> select it and click to download  
+    for csv_element in csv_elements_list: 
+        
+        #skip if the csv element has already been downloaded before     
+        #extract the date from the csv name 
+        regexp_date="(.*?) \d{2}\:\d{2}"
+        csv_date=re.findall(regexp_date,csv_element)[0]
+        csv_date=parser.parse(csv_date).date()
+        csv_date=str(csv_date)
+        check_duplicate=dataset_id+"-"+datatype+"-"+csv_date
+        
+        if check_duplicate not in list_already_in_db:
+            random_sleep(5)
+            try:
+                get_element_contains(driver,csv_element).click()
+                print(f"{csv_element} downloaded")
+            except:
+                # if error go back after getting the error
+                print(f"{csv_element} - error downloading csv")
+                        
+        else:    
+            print(f"-- element already in database {check_duplicate}")
+            pass
+        
+    # list the files that have been downloaded
+    csvfilenames =  glob.glob(os.path.join(downloadPath, "*.csv"))
+    #df_concat=pd.DataFrame(columns=['value','country','lon','lat','data_type','disaster_name','disaster_id','date'])
+    
+    # open the files one by one
+    #file_no=0
+    for csvfilename in csvfilenames:
+        df_csv=pd.read_csv(csvfilename)
+        #get the date from the csv_name
+        regexp_date="(\d{4}-\d{2}-\d{2})"
+        date_dataset=re.findall(regexp_date, csvfilename)[0]
+    
+        #change the column name
+        #data_type= [x for x in df_csv.columns if x in ('bingtiles','admin_regions') ][0]
+
+        #create new column with the datatype and datatype id 
+        df_csv['data_type']=datatype
+
+        #create new column with the name of the disaster
+        df_csv['disaster_name']=disaster_name
+        df_csv['disaster_id']=dataset_id
+        df_csv['date']=date_dataset
+
+        df_concat=pd.concat([df_concat,df_csv])
+        print(csvfilename+ ' concat')
+    return df_concat
+
+
+
+def scrape(dataset_id_list):
+    
+    #8 - for each dataset - open the url and download all the csvs 
+    today_date=datetime.now()
+    
+    #initialization of the browser 
+    driver = webdriver.Remote(command_executor = vm_public_ip, desired_capabilities = capabilities, options= options)
+    
+    #login initialization
+    login(driver, loginURL, loginName, loginPass)
+    
+    #for each dataset id: for loop
+    for dataset_id in dataset_id_list:
+        #start by making sure the download folder is empty
+        #delete everything in the download file 
+
+        files = glob.glob(downloadPath+'/*')
+        for f in files:
+            os.remove(f)    
+    
+        # open the dataset url 
+        print(f"--dataset id - {dataset_id}")
+        url= f'https://partners.facebook.com/data_for_good/data/{dataset_id}/files/?partner_id=3884468314953904'
+        driver.get(url)
+    
+        random_sleep(2)
+    
+        # get the disaster name 
+        htmlsource=driver.page_source
+        regexp_disaster_name="div aria-level=\"2.*?\" role=\"heading\">(.*?)<\/div><span class=\""
+        try:
+            disaster_name= re.findall(regexp_disaster_name, htmlsource)[0]
+        except:
+            disaster_name=''
+        print(disaster_name)
+    
+        # once we have the disaster name - check if it's in the EAPR region
+        # if not move to the next disaster and stop the computing
+    
+        if is_in_eapr(disaster_name)==0 :
+            print(f"-- disaster out of EAPR boundaries {disaster_name}")
+            pass
+    
+        else:
+    
+            # identify all the csvs and download them - first for the coverage
+            print ("---- Active coverage")
+    
+            random_sleep(2)
+            htmlsource=driver.page_source
+    
+            #get the datatype from the button
+            regexp_datatype="Network coverage: <strong>(.*?)</strong>"
+            datatype_from_regexp=re.findall(regexp_datatype,htmlsource)[0]
+            print(f'datatype from regexp: {datatype_from_regexp}')
+    
+            #translate the datatype into the real one used in the csvs
+    
+            if datatype_from_regexp=='Active':
+                datatype='coverage'
+            if datatype_from_regexp=='Undetected':
+                datatype='no_coverage'
+            if datatype_from_regexp=='Probability':
+                datatype='p_connectivity'
+    
+            df_concat=download_csvs(driver,datatype)
+            print('concat ok for 1st type')
+    
+            # then for the undetected 
+            # move to the no coverage and download the csvs
+            random_sleep(2)
+            
+            #find the network coverage button and click it
+            get_element_contains(driver,'Network coverage').click()
+    
+            #move up to go to the probability data
+            actions = ActionChains(driver)
+            actions.send_keys(Keys.UP)
+            actions.send_keys(Keys.ENTER)
+            actions.perform()
+    
+            #go back to the files list 
+            random_sleep(2)
+            #go back to the files tab
+            driver.find_element_by_xpath("//div[@aria-label='Files']").click()
+    
+            #concat the results with the previous results
+            print ("---- 2nd data type")
+    
+            random_sleep(2)
+            htmlsource=driver.page_source
+    
+            #get the datatype from the button
+            regexp_datatype="Network coverage: <strong>(.*?)</strong>"
+            datatype_from_regexp=re.findall(regexp_datatype,htmlsource)[0]
+            print(f'datatype from regexp: {datatype_from_regexp}')
+    
+            #translate the datatype into the real one used in the csvs
+    
+            if datatype_from_regexp=='Active':
+                datatype='coverage'
+    
+            if datatype_from_regexp=='Undetected':
+                datatype='no_coverage'
+    
+            if datatype_from_regexp=='Probability':
+                datatype='p_connectivity'
+    
+            df_concat=pd.concat([df_concat,download_csvs(driver,datatype)])
+            print('concat ok for 2nd type')
+            #print(len(df_concat))
+    
+    
+            # and finally for the probability 
+            # first go back to the first page to reinitialize the cursor position - there has been issues of labels placed differently for each disaster
+            driver.get(url)
+            random_sleep(2)
+            # move to the probability page and download the csvs
+    
+            #find the button and click it
+            get_element_contains(driver,'Network coverage').click()
+    
+            #move up to go to the No coverage data
+            actions = ActionChains(driver)
+            actions.send_keys(Keys.DOWN)
+            actions.send_keys(Keys.ENTER)
+            actions.perform()
+    
+            #go back to the files list 
+            random_sleep(3)
+    
+            #go back to the files tab
+            driver.find_element_by_xpath("//div[@aria-label='Files']").click()
+    
+            #concat the results with the previous results
+            print ("---- 3rd datatype")
+    
+            random_sleep(2)
+            htmlsource=driver.page_source
+    
+            #get the datatype from the button
+            regexp_datatype="Network coverage: <strong>(.*?)</strong>"
+            datatype_from_regexp=re.findall(regexp_datatype,htmlsource)[0]
+            print(f'datatype from regexp: {datatype_from_regexp}')
+            #translate the datatype into the real one used in the csvs
+    
+            if datatype_from_regexp=='Active':
+                datatype='coverage'
+            if datatype_from_regexp=='Undetected':
+                datatype='no_coverage'
+            if datatype_from_regexp=='Probability':
+                datatype='p_connectivity'
+    
+            df_concat=pd.concat([df_concat,download_csvs(driver,datatype)])
+            print('concat ok for 3rd type')
+            #print(len(df_concat))
+    
+            # if there is no data in df concat, meaning the datasets have all alreay been downloaded, we just stop
+            if len(df_concat)>0:
+                print(len(df_concat))
+                #get h3 for the lat lon 
+                df_concat['h3_08']=df_concat.apply(lambda x: get_h3(x['lat'], x['lon']), axis=1)
+                print('h3 ok')
+                
+                #drop lat/lon
+                df_concat=df_concat.drop(columns=['lat','lon'])
+    
+                #group by h3_08 and datatype - mean of the value
+                df_concat_backup=df_concat
+                df_concat=df_concat.groupby(['disaster_name','disaster_id','country','data_type','date', 'h3_08']).mean().reset_index()
+                
+                #add update date 
+                df_concat['update_date']=today_date
+    
+                #change the column names to align with the sql 
+                df_concat=df_concat.rename(columns={"disaster_id": "meta_disaster_id", "disaster_name": "meta_disaster_name"})
+    
+                df_concat_hex=df_concat[['meta_disaster_id','country','data_type','date', 'h3_08','update_date','value']]
+    
+                #save into postgres connectivity_hex
+                engine = create_engine(f'postgresql://{login_postgres}:{password_postgres}@postgres-fdtn.postgres.database.azure.com:5432/postgres')
+                print('engine created')
+    
+                df_concat_hex.to_sql('meta_connectivity_hex_test', if_exists='append' ,schema='private',con=engine,chunksize=500000, method='multi', index=False)
+                print('df_concat_hex - saved into postgres')
+                #save into postgres connectivity_disaster
+                # only if there is a new disaster
+    
+                if dataset_id not in list_disaster_already_in_db:
+                    df_concat_disaster=df_concat[['meta_disaster_name','meta_disaster_id']].groupby(['meta_disaster_name','meta_disaster_id']).count()
+                    print(df_concat_disaster)
+    
+                    df_concat_disaster.to_sql('meta_connectivity_disaster_test', if_exists='append',schema='private',con=engine,chunksize=500000, method='multi')
+                    print('df_concat_disaster - saved into postgres')
+    
+            #randomly regenerate a new browser to avoid detection by meta and re login 
+            rand_browser_int= randint(1,10) 
+            if rand_browser_int >= 7:
+                print('closing browser')
+                #close the driver
+                driver.quit()
+                # and reopen a new one + login
+                driver = webdriver.Remote(command_executor = vm_public_ip, desired_capabilities = capabilities, options= options)
+    
+                login(driver, loginURL, loginName, loginPass)
+                #stop the driver 
+            
+            random_sleep(5)
+
+
+
 def test():
     driver = webdriver.Remote(command_executor = vm_public_ip, desired_capabilities = capabilities, options= options)
     driver.fullscreen_window()
     login(driver, loginURL, loginName, loginPass)
     dataset_list=get_datasets(driver)
     print(dataset_list)
-    return dataset_list
+    scrape(dataset_id_list)
 
 with DAG(
     ## MANDATORY 
