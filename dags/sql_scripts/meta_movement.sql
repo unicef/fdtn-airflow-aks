765b99cff52164099f19372c101b45e528e5429f
huruizverastegui <118810504+huruizverastegui@users.noreply.github.com>
2023-09-14 09:45:25 +0700

    Update meta_movement.sql

--- dags/sql_scripts/meta_movement.sql
+++ dags/sql_scripts/meta_movement.sql
@@ -1,8 +1,8 @@
--- TRUNCATE TABLE AND INSERT
+-- set up public table with adm 0/1/2 levels 
 
-TRUNCATE public.meta_movement_formatted;
+TRUNCATE public.meta_movement;
 
-INSERT INTO public.meta_movement_formatted (
+INSERT INTO public.meta_movement(
 
 SELECT mm.disaster_id
 ,mm.disaster_name
@@ -28,4 +28,40 @@ on ah_end.h3_08= mm.end_h3_08
 group by 1,2,3,4,5,6,7,8,9,10)
 ;
 
+CREATE INDEX ON public.meta_movement(start_adm2)
+; 
+
+-- set up public table with adm 2 flows 
+-- keep only adm 2, data for the first 72hours, and movements >0
+
+
+TRUNCATE public.meta_movement_formatted;
+
+INSERT INTO public.meta_movement_formatted(
+
+with min_date as (
+select disaster_id
+, min(date_time) as start_date
+from public.meta_movement
+group by 1
+),
+
+grouped_data as 
+(select mm.disaster_id, start_date, disaster_name, start_adm2, end_adm2, sum(mm.n_difference) as total
+from public.meta_movement mm
+left join min_date md 
+on md.disaster_id=mm.disaster_id
+
+
+where date_time - start_date <= interval '3 days'
+and end_adm2 != start_adm2
+group by 1,2,3,4,5)
+
+select * from grouped_data
+where total >0
+)
+  
+;
+
 CREATE INDEX ON public.meta_movement_formatted(start_adm2)
+; 
