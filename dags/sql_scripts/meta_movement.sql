519b85eb894665780e955ae8c4e97a9079a01489
huruizverastegui <118810504+huruizverastegui@users.noreply.github.com>
2023-10-02 18:25:14 +0700

    Update meta_movement.sql

--- dags/sql_scripts/meta_movement.sql
+++ dags/sql_scripts/meta_movement.sql
@@ -3,11 +3,31 @@ TRUNCATE public.meta_population_crisis_adm2 ;
 
 -- admin2 mapping + group by 
 
-INSERT INTO public.meta_population_crisis_adm2(
-SELECT mpch.disaster_id
-,mpch.disaster_name
-,mpch.country
-,mpch.date
+INSERT INTO public.meta_population_crisis_adm2 ( 
+
+with population_meta_grouped_h308 as (
+
+select disaster_id, disaster_name, country, h3_08, date, sum(n_baseline) as n_baseline, sum(n_crisis) as n_crisis, sum(n_difference) as n_difference
+FROM private.meta_population_crisis_h308 mpch
+group by 1,2,3,4,5
+
+)
+,
+
+population_meta_joined_hrsl as ( 
+select pmg.*, pcd.general as hrsl_population 
+from population_meta_grouped_h308 pmg
+left join public.population_crosstab_deduplicated pcd
+on pmg.h3_08=pcd.h3_08
+)
+, 
+
+population_joined_adm as (
+
+SELECT pmjh.disaster_id
+,pmjh.disaster_name
+,pmjh.country
+,pmjh.date
 
 , ah.name0 as adm0
 , ah.name1 as adm1
@@ -17,17 +37,18 @@ SELECT mpch.disaster_id
 , ah.gid1 as gid1
 , ah.gid2 as gid2
 
-,sum(mpch.n_difference) as n_difference
-,sum(mpch.n_baseline) as n_baseline
-,sum(mpch.n_crisis) as n_crisis
+,sum(pmjh.n_difference) as n_difference
+,sum(pmjh.n_baseline) as n_baseline
+,sum(pmjh.n_crisis) as n_crisis
+,sum(pmjh.hrsl_population) as hrsl_population
 
-FROM private.meta_population_crisis_h308 mpch 
+FROM population_meta_joined_hrsl pmjh
 
 left join  public.adm2_hex ah
-on ah.h3_08= mpch.h3_08 
-
+on ah.h3_08= pmjh.h3_08 
 group by 1,2,3,4,5,6,7,8,9,10
-); 
+)
+select * from population_joined_adm ); 
 
 CREATE INDEX ON public.meta_population_crisis_adm2(amd2) ;
 
@@ -36,6 +57,7 @@ CREATE INDEX ON public.meta_population_crisis_adm2(amd2) ;
 TRUNCATE public.meta_population_crisis_formatted;
 
 INSERT INTO public.meta_population_crisis_formatted(
+
 
 with max_date_table as (
 select disaster_id
@@ -67,7 +89,14 @@ group by 1,2,3,4,5,6
 mapping_meta_final as (select * from mapping_meta_inter where rn=1) ,
 
 grouped_data as 
-(select mapping_meta_final.event_id, date, gdacs_name,meta_disaster_name , adm2, gid2, n_difference, n_baseline, n_crisis
+(select mapping_meta_final.event_id, date, gdacs_name,meta_disaster_name , adm2, gid2, n_difference, n_baseline, n_crisis, 
+
+ -- get the scaled difference comparing the HRSL and the meta population during crisis baseline
+case when hrsl_population<= n_baseline then n_difference
+else  n_difference *( hrsl_population/n_baseline ) 
+end 
+as n_difference_scaled
+
 from public.meta_population_crisis_adm2 mpca
 
 left join max_date_table md 
