700a0a71c02fa9384bc0ace8af24d17db5ed97eb
huruizverastegui <118810504+huruizverastegui@users.noreply.github.com>
2023-11-09 17:03:23 +0700

    Create test_mail.py

---
+++ dags/test_mail.py
@@ -0,0 +1,41 @@
+
+
+import airflow 
+from datetime import timedelta 
+from airflow import DAG 
+from datetime import datetime, timedelta 
+from airflow.operators.python_operator import PythonOperator 
+from airflow.operators.email_operator import EmailOperator
+
+
+## Arguments applied to the tasks, not the DAG in itself 
+default_args={
+    'owner':'airflow',
+    'email_on_failure': False,
+    'email_on_retry':False,
+    'retries':2,
+    'retry_delay': timedelta(minutes=1)
+}
+
+
+with DAG(
+    ## MANDATORY 
+    dag_id='mail_test',
+    start_date=datetime(2022,11,28),
+    default_args=default_args,
+    description='sitrep mail test',
+    #schedule not used for the moment as the DAGS run when airflow boots everymorning
+    #schedule_interval='0 2 * * *',
+    # no need to catch up on the previous runs
+    catchup=False
+) as dag:
+
+        send_email = EmailOperator( 
+        task_id='send_email', 
+        to='huruiz@unicef.org', 
+        subject='email test', 
+        html_content=" this has been a success ", 
+        dag=dag_email)
+
+
+        send_email
