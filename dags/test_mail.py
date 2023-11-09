6b1125649854eec68d4c035eca820953c51e7331
huruizverastegui <118810504+huruizverastegui@users.noreply.github.com>
2023-11-09 18:00:27 +0700

    Update test_mail.py

--- dags/test_mail.py
+++ dags/test_mail.py
@@ -19,7 +19,7 @@ default_args={
 
 with DAG(
     ## MANDATORY 
-    dag_id='sitrep_disasters',
+    dag_id='mail',
     start_date=datetime(2022,11,28),
     default_args=default_args,
     description='sitrep disasters',
