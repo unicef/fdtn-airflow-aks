af6049a8e0d44c176ce22e33d9a8e0fc5bd2c90e
huruizverastegui <118810504+huruizverastegui@users.noreply.github.com>
2023-11-13 16:22:28 +0700

    Update test_mail.py

--- dags/test_mail.py
+++ dags/test_mail.py
@@ -6,6 +6,9 @@ from airflow import DAG
 from datetime import datetime, timedelta 
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+
 from airflow.operators.python_operator import PythonOperator 
 from airflow.operators.email_operator import EmailOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
