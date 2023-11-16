2c98e331c9dd00c67381cf99c9b518db0c31ee9b
huruizverastegui <118810504+huruizverastegui@users.noreply.github.com>
2023-11-16 15:55:40 +0700

    Update disasters.py

--- dags/disasters.py
+++ dags/disasters.py
@@ -472,11 +472,11 @@ def get_latest_disasters_rss():
 
         
         subject = "Test email GDACS"
-        cc = ['huruiz@unicef.org','hugo.ruiz.verastegui@gmail.com']
+        cc = os.getenv('REQUEST_MAIL_META_CC')
         body = "Dear Anthony, <br> <br> I hope you are doing great and that Vientiane's croissants are exquisite <br> We just identified some new high intensity disaster in the East Asia Pacific Region and we would like to start the generation of the Population/ Movements/ Connectivity datasets for the following disaster(s):  <br> <br> "
-        sender = "unicef.data.eapro@gmail.com"
-        recipients = ["huruiz@unicef.org","amockler@unicef.org"]
-        password = "svdh gonx kfch jahb" 
+        sender = os.getenv('REQUEST_MAIL_META_FROM')
+        recipients = os.getenv('REQUEST_MAIL_META_TO')
+        password = os.getenv('REQUEST_MAIL_META_APP_PASSWORD')
 
         df_html = """\
         <html>
