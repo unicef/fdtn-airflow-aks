0195231c01d64d93ed184453f193c48c3aa6fd64
unicef <hugo.ruiz.verastegui@gmail.com>
2024-01-03 11:23:34 +0000

    Update from https://github.com/unicef/hd4d_ppt/commit/6e18325f5e33170e9c7852bbf612f0af91baf3d3

--- dags/sql_scripts/population_region.sql
+++ dags/sql_scripts/population_region.sql
@@ -1,31 +1,27 @@
 -- Create disaster table if does not exists
 -- coalesce to replace NULL by 0
-
-
 TRUNCATE population_by_region;
 
 INSERT INTO population_by_region (
+
 SELECT disasters_hex.event_id ,
-adm2_hex.gid2 AS gid2,
-eapro_adm2."NAME_2" AS "NAME_2", 
-coalesce(sum(pc.children_under_five),0) AS children_under_five, 
-coalesce(sum(pc.elderly_60_plus),0) AS elderly_60_plus, 
-coalesce(sum(pc.men),0) AS men, 
-coalesce(sum(pc.women),0) AS women, 
-coalesce(sum(pc.women_of_reproductive_age_15_49),0) AS women_of_reproductive_age_15_49, 
-coalesce(sum(pc.youth_15_24),0) AS youth_15_24, 
-coalesce(sum(pc.general),0) AS general, 
-coalesce(sum(schools.count),0) AS school_count ,
-coalesce(sum(hospitals.count),0) AS hospital_count 
+pi.gid2 AS gid2,
+pi."NAME_2" AS "NAME_2", 
+coalesce(sum(pi.children_under_five),0) AS children_under_five,
+coalesce(sum(pi.elderly_60_plus),0) AS elderly_60_plus, 
+coalesce(sum(pi.men),0) AS men, 
+coalesce(sum(pi.women),0) AS women, 
+coalesce(sum(pi.women_of_reproductive_age_15_49),0) AS women_of_reproductive_age_15_49, 
+coalesce(sum(pi.youth_15_24),0) AS youth_15_24, 
+coalesce(sum(pi.general),0) AS general,
+coalesce(sum(pi.school_count),0) AS school_count ,
+coalesce(sum(pi.hospital_count),0) AS hospital_count 
 
-FROM population_crosstab_deduplicated pc
-LEFT JOIN disasters_hex ON pc.h3_08 = disasters_hex.h3_08 
-LEFT JOIN adm2_hex ON disasters_hex.h3_08 = adm2_hex.h3_08 
-LEFT JOIN eapro_adm2 ON adm2_hex.gid2 = eapro_adm2."GID_2" 
-LEFT JOIN schools ON schools.h3_08 = adm2_hex.h3_08 
-LEFT JOIN hospitals ON hospitals.h3_08 = adm2_hex.h3_08 
+FROM disasters_hex 
+LEFT JOIN public.population_inter pi ON pi.h3_08 = disasters_hex.h3_08 
 
-GROUP BY disasters_hex.event_id, adm2_hex.gid2, eapro_adm2."NAME_2" 
+
+GROUP BY disasters_hex.event_id, pi.gid2, pi."NAME_2" 
 ORDER BY children_under_five DESC 
 );
 
