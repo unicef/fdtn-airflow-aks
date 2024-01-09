988b67d94b9f59606fe72fd2a352fd69a9fa47db
unicef <hugo.ruiz.verastegui@gmail.com>
2024-01-09 08:40:13 +0000

    Update from https://github.com/unicef/hd4d_ppt/commit/a7b602328dc61ad7ab15af9a3ddb5b62a1f79816

--- dags/sql_scripts/meta_movement.sql
+++ dags/sql_scripts/meta_movement.sql
@@ -129,7 +129,9 @@ on mpca.disaster_id=mapping_meta_final.meta_disaster_id
 
 where mpca.date_time = md.max_date )
 
-select * from grouped_data
+select gd.*, geometry from grouped_data gd
+LEFT JOIN public.eapro_adm2 ea 
+ON gd.gid2 =ea."GID_2"
 
 );
 CREATE INDEX ON public.meta_population_crisis_formatted(adm2)
