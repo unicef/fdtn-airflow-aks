75cdfa9875de6945cdc86d587488582947040e5c
unicef <hugo.ruiz.verastegui@gmail.com>
2024-02-05 10:52:41 +0000

    Update from https://github.com/unicef/hd4d_ppt/commit/1018886fd46823b40fe60cef92286e5719f2d309

--- dags/sql_scripts/meta_connectivity_formatting.sql
+++ dags/sql_scripts/meta_connectivity_formatting.sql
@@ -1,28 +1,23 @@
 -- TRUNCATE TABLE AND INSERT
 
-TRUNCATE public.meta_connectivity_formatted;
+TRUNCATE public.meta_connectivity_test_slider;
 
-INSERT INTO public.meta_connectivity_formatted (
-
-select event_id, h3_08, date_trunc('week', date) as week_start_date
-,
+INSERT INTO public.meta_connectivity_test_slider (
+select event_id, h3_08, date, 
 
 AVG(CASE WHEN data_type='coverage' then value 
 ELSE NULL 
-END) as "avg_coverage", 
+end) as "avg_coverage", 
 
 AVG(CASE WHEN data_type='no_coverage' then value 
 ELSE NULL 
-END) as "avg_no_coverage", 
+end) as "avg_no_coverage", 
 
 AVG(CASE WHEN data_type='p_connectivity' then value 
 ELSE NULL 
-END) as "avg_p_connectivity"
+end) as "avg_p_connectivity"
 
 FROM 
-(
-    
-select event_id, name, fromdate, todate, h3_08, data_type, date, value from 
 
 (select d_hex.event_id, d.name, d.fromdate, d.todate, d_hex.h3_08 , mch.data_type, mch.date, mch.value, mch.meta_disaster_id,
 row_number() over(partition by d_hex.event_id,d_hex.h3_08,mch.data_type order by date desc) as rn
@@ -41,11 +36,9 @@ and mch.date >= d.fromdate
 
 ) as sub
 
-where rn=1
 
-) as sub_2
 
 GROUP by 1,2,3)
 ;
 
-CREATE INDEX ON public.meta_connectivity_formatted(h3_08)
+CREATE INDEX ON public.meta_connectivity_test_slider(h3_08)
