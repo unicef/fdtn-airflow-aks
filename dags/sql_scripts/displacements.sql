555bcd3643342cb24c9f875da32d837743f9879d
unicef <hugo.ruiz.verastegui@gmail.com>
2024-03-19 08:47:54 +0000

    Update from https://github.com/unicef/hd4d_ppt/commit/fad55424179914edec838bb4638323e7523b0f02

--- dags/sql_scripts/displacements.sql
+++ dags/sql_scripts/displacements.sql
@@ -40,18 +40,19 @@ CREATE INDEX ON public.meta_movement(start_adm2);
 -- keep only adm 2, data for the first 72hours, and movements >0
 
 
+
 TRUNCATE public.meta_movement_formatted;
 
 INSERT INTO public.meta_movement_formatted(
 
-with min_date as (
-select disaster_id
-, min(date_time) as start_date
-from public.meta_movement
-group by 1
-),
+--with min_date as (
+--select disaster_id
+--, min(date_time) as start_date
+--from public.meta_movement
+--group by 1
+--),
 
-mapping_meta_inter as (
+with mapping_meta_inter as (
 select dh.event_id, d.name,  d.fromdate, d.todate, mmh.disaster_id , mmh.data_type, count(*) as occurences
 
  ,row_number() over(partition by mmh.disaster_id order by count(*) desc) as rn
@@ -66,37 +67,40 @@ on mmh.start_h3_08=dh.h3_08
 
 where TRUE 
 and mmh.date_time<= d.todate + INTERVAL '28 day' 
-and mmh.date_time >= d.fromdate - INTERVAL '3 day'
+and mmh.date_time >= d.fromdate - INTERVAL '7 day'
 and mmh.disaster_id is not null
 group by 1,2,3,4,5,6
 ), 
 
 mapping_meta_final as (select * from mapping_meta_inter where rn=1) ,
 
+
+admin_2_disaster as ( select event_id, gid2 from disasters_hex dh  group by 1,2),
+
 grouped_data as 
-(select mapping_meta_final.event_id, start_date, disaster_name, start_adm2, end_adm2, sum(mm.n_difference) as total
+(select mapping_meta_final.event_id, date_time , disaster_name, start_adm2, end_adm2, sum(mm.n_difference) as total
 from public.meta_movement mm
 
-left join min_date md 
-on md.disaster_id=mm.disaster_id
+--left join min_date md 
+--on md.disaster_id=mm.disaster_id
 
 left join mapping_meta_final
 on mm.disaster_id=mapping_meta_final.disaster_id
 
 
 -- inner join to make sure we only keep movement starting from the gid2 of the disaster's zone 
-inner join  disasters_hex dh 
-on  ( dh.event_id = mapping_meta_final.event_id and dh.gid2=mm.start_gid2)
+inner join  admin_2_disaster a2d
+on  ( a2d.event_id = mapping_meta_final.event_id and a2d.gid2=mm.start_gid2)
 
 
-where date_time - start_date <= interval '3 days'
+where TRUE 
+-- and date_time - start_date <= interval '3 days'
 and end_adm2 != start_adm2
 group by 1,2,3,4,5)
 
 select * from grouped_data
 where total >0
-)
+);
 
-;
 
 CREATE INDEX ON public.meta_movement_formatted(start_adm2);
